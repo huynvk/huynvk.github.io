aca80ab0c2abc7834d5885613ce05bd539d553df
Huy Ngo <huy.ngo@codelink.io>
2019-10-15 15:33:27 +0700

    Remove .contentful.json dependency

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,9 @@
 require("dotenv").config({ path: `.env` })
 
-let contentfulConfig
-
-try {
-  // Load the Contentful config from the .contentful.json
-  contentfulConfig = require("./.contentful")
-} catch (_) {}
-
 // Overwrite the Contentful config with environment variables if they exist
-contentfulConfig = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
-  accessToken:
-    process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN,
 }
 
 const { spaceId, accessToken } = contentfulConfig
